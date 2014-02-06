93eb6739d649654621b69e1f574fba682cd9f373
Mårten Gustafson <marten.gustafson@gmail.com>
2014-02-06 11:12:52 +0100

    Move Jackson stuff into service

--- src/main/java/chids/SnsOrchestrateProxy.java
+++ src/main/java/chids/SnsOrchestrateProxy.java
@@ -1,5 +1,16 @@
 package chids;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
@@ -18,5 +29,23 @@ public class SnsOrchestrateProxy extends Service<Configuration> {
 
     public static void main(final String[] args) throws Exception {
         new SnsOrchestrateProxy().run(args);
+    }
+
+    public static final ObjectMapper JSON = configure(new ObjectMapper(new JsonFactory()));
+
+    public static ObjectMapper configure(final ObjectMapper mapper) {
+        return mapper
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(JsonParser.Feature.ALLOW_COMMENTS, false)
+                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .setVisibility(PropertyAccessor.CREATOR, JsonAutoDetect.Visibility.ANY)
+                .setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
+                .setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE)
+                .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
+                .setAnnotationIntrospector(new JacksonAnnotationIntrospector())
+                .registerModule(new JodaModule())
+                .registerModule(new GuavaModule())
+                .setDateFormat(new ISO8601DateFormat());
     }
 }
--- src/main/java/chids/TextPlainAsJson.java
+++ src/main/java/chids/TextPlainAsJson.java
@@ -1,5 +1,6 @@
 package chids;
 
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
 
 import java.io.IOException;
@@ -7,26 +8,17 @@ import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
-import com.fasterxml.jackson.datatype.guava.GuavaModule;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 @Provider
+@Consumes(TEXT_PLAIN)
 public class TextPlainAsJson implements MessageBodyReader<JsonNode> {
 
     @Override
@@ -44,24 +36,6 @@ public class TextPlainAsJson implements MessageBodyReader<JsonNode> {
                              final MediaType mime,
                              final MultivaluedMap<String, String> headers,
                              final InputStream entity) throws IOException, WebApplicationException {
-        return MAPPER.readTree(entity);
-    }
-
-    private static final ObjectMapper MAPPER = configure(new ObjectMapper(new JsonFactory()));
-
-    public static ObjectMapper configure(final ObjectMapper mapper) {
-        return mapper
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .configure(JsonParser.Feature.ALLOW_COMMENTS, false)
-                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-                .setVisibility(PropertyAccessor.CREATOR, JsonAutoDetect.Visibility.ANY)
-                .setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
-                .setVisibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE)
-                .setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
-                .setAnnotationIntrospector(new JacksonAnnotationIntrospector())
-                .registerModule(new JodaModule())
-                .registerModule(new GuavaModule())
-                .setDateFormat(new ISO8601DateFormat());
+        return SnsOrchestrateProxy.JSON.readTree(entity);
     }
 }
